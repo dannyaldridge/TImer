310b0f5f0b031491f90089dc5dc23bc1841d9273
Danny Aldridge <dannyaldridge@gmail.com>
2013-08-20 15:56:38 +0100

    check boxes

--- 1-starter-kit/public/javascripts/application.js
+++ 1-starter-kit/public/javascripts/application.js
@@ -1,4 +1,38 @@
+jQuery.fn.extend({
+
+	taskStates:['task-empty', 'task-x', 'task-apostrophie', 'task-dash'],
+
+	resetTaskStateClassNames:function() {
+		var elements = this;
+		jQuery.each(jQuery.fn.taskStates, function() {
+			elements.removeClass(this);	
+		})
+		return this;
+	},
+
+	resetTaskState:function() { },
+
+	toggleTaskState:function() {
+		this.resetTaskStateClassNames();
+
+		return this.each(function() {
+			var element = jQuery(this);
+			var taskStateIndex = element.data('taskStateIndex') || 0;
+			taskStateIndex = (taskStateIndex + 1) % jQuery.fn.taskStates.length;
+
+			element.data('taskStateIndex', taskStateIndex)
+				.addClass(jQuery.fn.taskStates[taskStateIndex]);
+		});
+	},
+
+});
+
 jQuery(function () {
+
+	$('.completion a').live("click", function(e) {
+		$(this).toggleTaskState();
+		return false;
+	});
 
 	$('#add').click(function(e) { 
 		var taskItem = $('#tasks ul li:first').clone();
